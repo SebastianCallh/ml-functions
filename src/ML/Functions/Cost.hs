3d50d6abb74561b1437f97955d30f8781d633b5a
Sebastian <sebastian.callh@gmail.com>
2018-02-13 16:21:02 +0100

    intial commit

---
+++ src/ML/Functions/Cost.hs
@@ -0,0 +1,20 @@
+{-|
+Module      : ML.Functions.Cost
+Description : This module contains common cost function for machine learning models.
+Copyright   : (c) Sebastian Callh, 2018
+License     : GPL-3
+Maintainer  : sebastian.callh@gmail.com
+Stability   : experimental
+Portability : POSIX
+
+This module contains common cost function for machine learning models.
+-}
+module ML.Functions.Cost
+  (
+  ) where
+
+import qualified Data.Vector.Storable as V
+
+--import
+
+--mse ::
